8495b9491c596fd961c8f367f8de45b522ed63ef
idkcrab <frish454@gmail.com>
2025-05-31 20:33:08 -0600

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "crab.GTMessanger";
+        public const string Name = "GTMessanger";
+        public const string Description = "FOR THE LAST TIME crab IS SPELT ALL LOWERCASE IT'S MY NAME NOT Crab IT'S crab";
+        public const string Version = "1.0.0";
+    }
+}
